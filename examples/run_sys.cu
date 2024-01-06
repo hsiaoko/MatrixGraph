6cbfed94b234b030a97a98158d28dcfa317d0a44
hsiaoko <xiaoke.zhu@outlook.com>
2024-01-06 21:43:41 +0800

    Implemented TileSpGEMM.

--- examples/run_sys.cu
+++ examples/run_sys.cu
@@ -22,16 +22,13 @@
 #include "core/gpu/global_func.cuh"
 #include "core/matrixgraph.cuh"
 
-DEFINE_string(data_l, "", "input dir 2.");
-DEFINE_string(data_r, "", "input dir 1.");
+DEFINE_string(data, "", "data dir path.");
 
-DEFINE_string(rule_dir, "", "root path of rules.");
 DEFINE_string(o, "", "output path.");
 DEFINE_string(sep, ",", "separator to split a line of csv file.");
 DEFINE_string(scheduler, "CHBL", "scheduler type.");
 DEFINE_bool(read_header, false, "whether to read header of csv.");
 DEFINE_uint64(n_partitions, 1, "number of partitions.");
-DEFINE_uint64(prefix_hash_predicate_index, INT_MAX, "number of partitions.");
 
 using sics::matrixgraph::core::components::scheduler::kCHBL;
 using sics::matrixgraph::core::components::scheduler::kEvenSplit;
@@ -52,11 +49,9 @@ int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   auto scheduler_type = Scheduler2Enum(FLAGS_scheduler);
-  sics::matrixgraph::core::HyperBlocker hb(
-      FLAGS_rule_dir, FLAGS_data_l, FLAGS_data_r, FLAGS_o, FLAGS_n_partitions,
-      FLAGS_prefix_hash_predicate_index, FLAGS_sep, scheduler_type);
+  sics::matrixgraph::core::MatrixGraph system(FLAGS_data, scheduler_type);
 
-  hb.Run();
+  system.Run();
 
   gflags::ShutDownCommandLineFlags();
   return EXIT_SUCCESS;
